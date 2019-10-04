3f96f1a6f22244cc2c439dd0ea666dbe375ce704
Megan Tiu <megantiu@me.com>
2019-10-04 18:23:42 -0400

    Add additional Record validations
    * Only allow type to be of a limited record set. This is
    because other types of Records have more fields and are more
    complicated, so we can't handle them at this time.
    * Validate that a Zone can only have one Record of each type. This is
    a limitation on NS1's end.

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,9 @@
 class Record < ApplicationRecord
+  TYPES_OF = %w(A AAAA ALIAS CNAME NS PTR SPF TXT).freeze
+
   belongs_to :zone
   validates_presence_of :type_of
   validates_presence_of :rdata
+  validates_inclusion_of :type_of, in: TYPES_OF
+  validates_uniqueness_of :type_of, scope: :zone_id
 end
